878fb9cda00a8c7d9215fbece0b8a89dbeeb7f42
Adam Osiecki <osiecki.adam@outlook.com>
2022-09-18 17:30:19 +0200

    display static image on layout

--- force-app/main/default/lwc/hangman/game.js
+++ force-app/main/default/lwc/hangman/game.js
@@ -1,7 +1,12 @@
+import ROUND_IMAGES from '@salesforce/resourceUrl/hangmanResources';
 class Game {
 
     constructor() {
         console.log('Hangman game is starting');
+    }
+
+    get imageUrl() {
+        return ROUND_IMAGES + '/images/round-7.png';
     }
 }
 
--- force-app/main/default/lwc/hangman/hangman.js
+++ force-app/main/default/lwc/hangman/hangman.js
@@ -2,8 +2,10 @@ import { LightningElement } from 'lwc';
 import { Game } from './game.js';
 
 export default class Hangman extends LightningElement {
+
     connectedCallback() {
         console.log('CONNECTED CALLBACK');
         const game = new Game();
+        this.imageUrl = game.imageUrl;
     }
 }
